f20e00cbfc5a1931f61ca26d75579dc874f8bc8d
Pietroks <131334815+Pietroks@users.noreply.github.com>
2025-06-17 16:07:27 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import store from './store'; // Certifique-se de que esse caminho está correto
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
+store.dispatch('user/checkLoginStatus');
+
 const app = createApp(App);
 app.use(router);
 app.use(store);
